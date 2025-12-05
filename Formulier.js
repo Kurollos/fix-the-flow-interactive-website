3c44302234a02fabb491e9c6b509a18e2f52a195
Kurollos <144329418+Kurollos@users.noreply.github.com>
2025-12-05 09:40:32 +0100

    mijn user story formulier stap 1 code #17

---
+++ Formulier.js
@@ -0,0 +1,23 @@
+/* ============================
+   INTERACTIEF CATALOGUS FORMULIER
+   ============================ */
+
+// ----------- VELD SELECTORS -----------
+const titelInput = document.getElementById("titel");
+const auteurInput = document.getElementById("auteur");
+const jaarInput = document.getElementById("jaar");
+const themaInput = document.getElementById("thema");
+
+const previewTitel = document.getElementById("preview-titel");
+const previewAuteur = document.getElementById("preview-auteur");
+const previewJaar = document.getElementById("preview-jaar");
+const previewTags = document.getElementById("preview-tags");
+const previewImages = document.getElementById("preview-images");
+
+const dropArea = document.getElementById("drop-area");
+const imageUpload = document.getElementById("image-upload");
+
+const saveBtn = document.getElementById("save-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+
+
